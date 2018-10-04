6f0910b161a2ac071ae3a9eb27167dffd1416ac3
Patrik Maunus <subscription@skriptladan.se>
2018-10-04 21:23:38 +0200

    DiceExpression can build a complex expression like 5d12+5

---
+++ Expression/Constant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMTools.Dice.Expression
+{
+    public class Constant : IDiceExpression
+    {
+        public Constant(int constant)
+        {
+            _constant = constant;
+        }
+
+        public int Evaluate()
+        {
+            return _constant;
+        }
+
+        private readonly int _constant;
+    }
+}
---
+++ Expression/DiceValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMTools.Dice.Expression
+{
+    public class DiceValue : IDiceExpression
+    {
+        public DiceValue(DiceRoller roller)
+        {
+            _diceRoller = roller;
+        }
+
+        public int Evaluate()
+        {
+            List<int> rolls = _diceRoller.Roll();
+            return rolls.Aggregate((lhs, rhs) => lhs + rhs);
+        }
+
+        private readonly DiceRoller _diceRoller;
+    }
+}
