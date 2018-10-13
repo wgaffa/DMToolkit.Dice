0337b27f6b30f3211da335142f38cf2ed1b9b379
Patrik Maunus <subscription@skriptladan.se>
2018-10-13 15:54:40 +0200

    Tests for parsing a Dice expression

--- DiceExpressionParseTest.cs
+++ DiceExpressionParseTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using DMTools.Dice;
+using DMTools.Dice.Parser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Superpower;
 using Superpower.Model;
@@ -15,5 +16,53 @@ namespace DiceTest
     [TestClass]
     public class DiceExpressionParseTest
     {
+        [TestMethod]
+        public void ParseSimpleDiceExpression()
+        {
+            string input = "1d20";
+
+            var tokenizer = new DiceExpressionTokenizer();
+            var tokens = tokenizer.Tokenize(input);
+            var expr = new DiceExpressionParser(new MockRandomGenerator(11)).Lambda.Parse(tokens);
+
+            Assert.AreEqual(11, expr.Compile()());
+        }
+
+        [TestMethod]
+        public void ParseArithmeticDiceExpression()
+        {
+            string input = "1d6 + 1d4";
+
+            var tokenizer = new DiceExpressionTokenizer();
+            var tokens = tokenizer.Tokenize(input);
+            var expr = new DiceExpressionParser(new MockRandomGenerator(3)).Lambda.Parse(tokens);
+
+            Assert.AreEqual(6, expr.Compile()());
+        }
+
+        [TestMethod]
+        public void ParseArithmeticDiceConstantExpression()
+        {
+            string input = "1d8 * 1.5";
+
+            var tokenizer = new DiceExpressionTokenizer();
+            var tokens = tokenizer.Tokenize(input);
+            var expr = new DiceExpressionParser(new MockRandomGenerator(6)).Lambda.Parse(tokens);
+
+            Assert.AreEqual(9, expr.Compile()());
+        }
+
+        [TestMethod]
+        public void ParseArithmeticIntegerExpression()
+        {
+            string input = "1d8 * 3";
+
+            var tokenizer = new DiceExpressionTokenizer();
+            var tokens = tokenizer.Tokenize(input);
+            var expr = new DiceExpressionParser(new MockRandomGenerator(6)).Lambda.Parse(tokens);
+
+            Assert.AreEqual(18, expr.Compile()());
+
+        }
     }
 }
