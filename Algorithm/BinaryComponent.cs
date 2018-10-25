f9371ce8274587e61dfe7d7b6212ebb39ab4e022
Patrik Maunus <subscription@skriptladan.se>
2018-10-25 18:58:47 +0200

    Detailed dice parser

--- Algorithm/BinaryComponent.cs
+++ Algorithm/BinaryComponent.cs
@@ -13,6 +13,31 @@ namespace DMTools.Dice.Algorithm
             _left = left;
         }
 
+        public static IComponent MakeBinary(OperatorType op, IComponent left, IComponent right)
+        {
+            IComponent result = null;
+
+            switch (op)
+            {
+                case OperatorType.Addition:
+                    result = new Adder(left, right);
+                    break;
+                case OperatorType.Subtraction:
+                    result = new Subtracter(left, right);
+                    break;
+                case OperatorType.Multiplication:
+                    result = new Multiplier(left, right);
+                    break;
+                case OperatorType.Division:
+                    result = new Divider(left, right);
+                    break;
+                default:
+                    throw new ArgumentException("operator type not supported", "op");
+            }
+
+            return result;
+        }
+
         protected IComponent _left;
     }
 }
