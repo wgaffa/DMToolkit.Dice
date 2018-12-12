e0f609891fad07065f107bda0533fc4aa00d1b76
Patrik Maunus <subscription@skriptladan.se>
2018-12-12 16:45:02 +0100

    Delete DiceRoller and DiceResult
    - DiceRoller was replaced with an extensible Dice.Term
    - DiceResult is now an IEnumrable<int> from Dice.Term.GetResults()
    method

--- Dice.Test/ConceptTests.cs
+++ Dice.Test/ConceptTests.cs
@@ -35,20 +35,6 @@ namespace DiceTest
         }
 
         [TestMethod]
-        public void SimpleParserDiceConcept()
-        {
-            TextParser<DiceRoller> diceParser =
-                from rolls in Numerics.Natural.OptionalOrDefault(new TextSpan("1"))
-                from _ in Character.In(new char[] { 'd', 'D' })
-                from sides in Numerics.Natural
-                select new DiceRoller(int.Parse(rolls.ToString()), new Dice(int.Parse(sides.ToString()), new MockRandomGenerator()));
-
-            var dice = diceParser.Parse("d10");
-
-            CollectionAssert.AreEqual(new List<int> { 5 }, dice.Roll().IndividualRolls);
-        }
-
-        [TestMethod]
         public void ExpressionSimpleConcept()
         {
             Expression constant5 = Expression.Constant(5);
@@ -75,29 +61,6 @@ namespace DiceTest
 
             var expected = (Func<int>)expr.Compile();
             Assert.AreEqual(5, expected());
-        }
-
-        [TestMethod]
-        public void ExpressionRollWithArithmeticConcept()
-        {
-            Dice twelveSidedDice = new Dice(12, new MockRandomGenerator());
-            DiceRoller diceCup = new DiceRoller(3, twelveSidedDice);
-
-            var rollMethod = typeof(DiceRoller).GetMethod("Roll");
-            Expression instance = Expression.Constant(diceCup);
-            Expression rollCall = Expression.Call(instance, rollMethod);
-            Expression diceResult = Expression.Property(rollCall, "Result");
-            ParameterExpression left = Expression.Variable(typeof(int), "left");
-            Expression assignRoll = Expression.Assign(left, diceResult);
-
-            Expression constant7 = Expression.Constant(7);
-            Expression addition = Expression.MakeBinary(ExpressionType.Add, assignRoll, constant7);
-            Expression block = Expression.Block(new[] { left }, addition);
-            LambdaExpression lambda = Expression.Lambda(block);
-
-            var expected = (Func<int>)lambda.Compile();
-
-            Assert.AreEqual(22, expected());
         }
     }
 }
--- Dice.Test/DiceRolling.cs
+++ Dice.Test/DiceRolling.cs
@@ -36,47 +36,5 @@ namespace DiceTest
         {
             Assert.ThrowsException<ArgumentException>(() => new Dice(0, new MockRandomGenerator()));
         }
-
-        [TestMethod]
-        public void RollSeveralDiceString()
-        {
-            DiceRoller diceCup = new DiceRoller("5d12", new MockRandomGenerator());
-
-            CollectionAssert.AreEqual(new List<int> { 5, 5, 5, 5, 5 }, diceCup.Roll().IndividualRolls);
-        }
-
-        [TestMethod]
-        public void CreateDiceRollerNegativeAmount()
-        {
-            Assert.ThrowsException<ArgumentException>(() => new DiceRoller("-5d12", new MockRandomGenerator()));
-        }
-
-        [TestMethod]
-        public void CreateDiceRollerNegativeSides()
-        {
-            Assert.ThrowsException<ArgumentException>(() => new DiceRoller("5d-6", new MockRandomGenerator()));
-        }
-
-        [TestMethod]
-        public void RollSeveralDice()
-        {
-            Dice d12 = new Dice(12, new MockRandomGenerator());
-            DiceRoller diceCup = new DiceRoller(5, d12);
-
-            CollectionAssert.AreEqual(new List<int> { 5, 5, 5, 5, 5 }, diceCup.Roll().IndividualRolls);
-        }
-
-        [TestMethod]
-        public void ResultImmutable()
-        {
-            List<int> rollList = new List<int>() { 5, 2, 7, 12 };
-            DiceResult diceResult = new DiceResult(rollList);
-
-            rollList[2] = 10;
-
-            List<int> expected = new List<int>() { 5, 2, 7, 12 };
-
-            CollectionAssert.AreEqual(expected, diceResult.IndividualRolls);
-        }
     }
 }
