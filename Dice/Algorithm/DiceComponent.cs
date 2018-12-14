eade1851705962b46c44a102496be848e9c98754
Patrik Maunus <subscription@skriptladan.se>
2018-12-14 22:50:15 +0100

    Fix a bug that ToString used different values
    - ToString and Calculate got different random numbers, fixed so that the
    diceterm is calculated on initialilize.

--- Dice/Algorithm/DiceComponent.cs
+++ Dice/Algorithm/DiceComponent.cs
@@ -1,6 +1,7 @@
 ﻿using DMTools.Die.Term;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,18 +13,22 @@ namespace DMTools.Die.Algorithm
         public DiceComponent(IDiceTerm diceTerm)
         {
             _diceTerm = diceTerm ?? throw new ArgumentNullException(nameof(diceTerm));
+            _currentRoll = _diceTerm.GetResults().ToList();
         }
 
         public double Calculate()
         {
-            return _diceTerm.GetResults().Sum();
+            return _currentRoll.Sum();
         }
 
         public override string ToString()
         {
-            return "[" + String.Join(", ", _diceTerm.GetResults()) + "]";
+            Debug.Assert(_currentRoll != null);
+
+            return "[" + String.Join(", ", _currentRoll) + "]";
         }
 
         private IDiceTerm _diceTerm;
+        private List<int> _currentRoll;
     }
 }
