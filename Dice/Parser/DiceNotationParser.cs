22a2b7ae75f0dce54d277808ce7256a062f6005e
Patrik Maunus <subscription@skriptladan.se>
2020-02-26 21:06:22 +0100

    Create parser for DiceNotation
    - Parse numbers

---
+++ Dice/Parser/DiceNotationParser.cs
@@ -0,0 +1,18 @@
+﻿using Superpower;
+using Superpower.Parsers;
+using System.Linq;
+using Wgaffa.DMToolkit.Expressions;
+
+namespace Wgaffa.DMToolkit.Parser
+{
+    public static class DiceNotationParser
+    {
+        private static readonly TokenListParser<DiceNotationToken, IExpression> Constant =
+            Token.EqualTo(DiceNotationToken.Number)
+            .Apply(Numerics.DecimalDouble)
+            .Select(n => (IExpression)new NumberExpression((float)n));
+
+        public static TokenListParser<DiceNotationToken, IExpression> Lambda =
+            Constant.AtEnd();
+    }
+}
---
+++ Tests/DiceNotationParserTests/DiceNotationParserTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Superpower;
+using System;
+using System.Collections;
+using Wgaffa.DMToolkit.Expressions;
+using Wgaffa.DMToolkit.Parser;
+
+namespace DiceNotationParserTests
+{
+    public class DiceNotationParserTests
+    {
+        public class ValidParseTestCaseData : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new TestCaseData("5")
+                    .Returns(typeof(NumberExpression));
+                yield return new TestCaseData("5.2")
+                    .Returns(typeof(NumberExpression));
+            }
+        }
+
+        [TestCaseSource(typeof(ValidParseTestCaseData))]
+        public Type Parse_ShouldSucceed(string input)
+        {
+            var tokenizer = new DiceNotationTokenizer();
+            var result = DiceNotationParser.Lambda.Parse(tokenizer.Tokenize(input));
+
+            return result.GetType();
+        }
+    }
+}
