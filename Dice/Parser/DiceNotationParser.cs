dc25c56d9f33fe5b8b83ef63f7a708b8fbcc9e5c
Patrik Maunus <subscription@skriptladan.se>
2020-02-27 05:39:55 +0100

    Add parsing of dice tokens

--- Dice/Parser/DiceNotationParser.cs
+++ Dice/Parser/DiceNotationParser.cs
@@ -1,4 +1,5 @@
 ﻿using Superpower;
+using Superpower.Model;
 using Superpower.Parsers;
 using System.Linq;
 using Wgaffa.DMToolkit.Expressions;
@@ -7,12 +8,27 @@ namespace Wgaffa.DMToolkit.Parser
 {
     public static class DiceNotationParser
     {
+        private static readonly TextParser<IExpression> DiceParser =
+            from rolls in Numerics.IntegerInt32.OptionalOrDefault(1)
+            from _ in Character.EqualTo('d')
+            from sides in Numerics.IntegerInt32
+            select (IExpression)new DiceExpression(new Dice(sides), rolls);
+
         private static readonly TokenListParser<DiceNotationToken, IExpression> Constant =
             Token.EqualTo(DiceNotationToken.Number)
             .Apply(Numerics.DecimalDouble)
-            .Select(n => (IExpression)new NumberExpression((float)n));
+            .Select(n => (IExpression)new NumberExpression((float)n))
+            .Named("constant");
 
-        public static TokenListParser<DiceNotationToken, IExpression> Lambda =
-            Constant.AtEnd();
+        private static readonly TokenListParser<DiceNotationToken, IExpression> DiceExpression =
+            Token.EqualTo(DiceNotationToken.Dice)
+            .Apply(DiceParser)
+            .Named("dice");
+
+        public static readonly TokenListParser<DiceNotationToken, IExpression> Expr =
+            DiceExpression.Or(Constant);
+
+        public static TokenListParser<DiceNotationToken, IExpression> Notation =
+            Expr.AtEnd();
     }
 }
--- Tests/DiceNotationParserTests/DiceNotationParserTests.cs
+++ Tests/DiceNotationParserTests/DiceNotationParserTests.cs
@@ -17,14 +17,18 @@ namespace DiceNotationParserTests
                     .Returns(typeof(NumberExpression));
                 yield return new TestCaseData("5.2")
                     .Returns(typeof(NumberExpression));
+                yield return new TestCaseData("3d8")
+                    .Returns(typeof(DiceExpression));
+                yield return new TestCaseData("d6")
+                    .Returns(typeof(DiceExpression));
             }
         }
 
         [TestCaseSource(typeof(ValidParseTestCaseData))]
         public Type Parse_ShouldSucceed(string input)
         {
-            var tokenizer = new DiceNotationTokenizer();
-            var result = DiceNotationParser.Lambda.Parse(tokenizer.Tokenize(input));
+            var tokens = new DiceNotationTokenizer().Tokenize(input);
+            var result = DiceNotationParser.Notation.Parse(tokens);
 
             return result.GetType();
         }
